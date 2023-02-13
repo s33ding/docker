340cffccfecef2fac21457bd746347aa9e6e5a68
s33ding <robertomdiniz@protonmail.com>
2023-02-13 14:20:49 -0300

    saving

--- hadoop/p2_run.sh
+++ hadoop/p2_run.sh
@@ -1,1 +1,2 @@
-docker run -it sequenceiq/hadoop-docker:2.7.0 /etc/bootstrap.sh -bash
+docker run -it -p 50070:50070 -p 8020:8020 -p 8030:8030 -p 8031:8031 -p 8032:8032 -p 8033:8033 -p 8040:8040 -p 8042:8042 -p 8088:8088 -p 19888:19888 -h hadoop --name hadoop-container sequenceiq/hadoop-docker
+
