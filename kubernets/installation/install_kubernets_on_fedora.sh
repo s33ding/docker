76349e4ab891b873bafb18e4b835c2c2b99cac70
s33ding <robertomdiniz@protonmail.com>
2024-05-04 19:08:21 -0300

    saving

--- kubernets/installation/install_kubernets_on_fedora.sh
+++
@@ -1,4 +0,0 @@
-echo "running:"
-echo "sudo dnf install kubernetes kubernetes-kubeadm kubernetes-client"
-sudo dnf install kubernetes kubernetes-kubeadm kubernetes-client
-echo "read this: https://docs.fedoraproject.org/en-US/quick-docs/using-kubernetes/"
